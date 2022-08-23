8e6aa9542e183a012b7d9e47492322ee9c335dff
Yahya <91276692+myakhlaqi@users.noreply.github.com>
2022-08-24 00:03:04 +0800

    Update Binomialdistribution.py

--- dist_gb/src/Binomialdistribution.py
+++ dist_gb/src/Binomialdistribution.py
@@ -47,7 +47,7 @@ class Binomial(Distribution):
         self.stdev = math.sqrt(self.n * self.p * (1 - self.p))
         return self.mean
 
-    # TODO:write a calculate_stdev() method accordin to the specifications below.
+    # TODO:
     def calculate_stdev(self):
         """Function to calculate the standard deviation from p and n.
 
