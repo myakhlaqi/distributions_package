e2cebc65564ffc49ee6cef3bfa0d50e6a2ac1c9f
Yahya <91276692+myakhlaqi@users.noreply.github.com>
2022-09-03 23:06:14 +0800

    Update Binomialdistribution.py

--- dist_gb/src/Binomialdistribution.py
+++ dist_gb/src/Binomialdistribution.py
@@ -69,7 +69,6 @@ class Binomial(Distribution):
     #           updates the mean attribute
     #           updates the standard deviation attribute
     #
-    #       Hint: You can use the calculate_mean() and calculate_stdev() methods
     #           defined previously.
     def replace_stats_with_data(self):
         """Function to calculate p and n from the data set. The function updates the p and n variables of the object.
