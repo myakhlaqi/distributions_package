63b9e5baa65c8a18a816fcc100742b9731251e6d
Yahya <91276692+myakhlaqi@users.noreply.github.com>
2022-10-02 15:44:31 +0800

    Update Binomialdistribution.py

--- dist_gb/src/Binomialdistribution.py
+++ dist_gb/src/Binomialdistribution.py
@@ -135,7 +135,7 @@ class Binomial(Distribution):
 
 
 
-      #   Be sure to label the bar chart with a title, x 
+      #   Be sure to label the bar chart with a 
 
       #   This method should also return the x and y values used to make the chart
       
