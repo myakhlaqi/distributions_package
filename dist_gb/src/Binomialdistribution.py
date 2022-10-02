5ee28e705b42d7adb0363ed73045965c3813d1da
Yahya <91276692+myakhlaqi@users.noreply.github.com>
2022-10-02 15:45:06 +0800

    Update Binomialdistribution.py

--- dist_gb/src/Binomialdistribution.py
+++ dist_gb/src/Binomialdistribution.py
@@ -13,7 +13,6 @@ class Binomial(Distribution):
         p (float) representing the probability of an event occurring
 
     """
-    #
     #       For example, if you flip a fair coin 25 times,
     #       You can then calculate the mean and standard deviation with the following formula:
     #           mean = p * n
